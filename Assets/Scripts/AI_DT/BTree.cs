6bb9821a6b60037ccaeb320abf0c5e2a63a1a377
Zac Barlow <zacbarlow14@gmail.com>
2024-10-12 11:37:53 -0700

    Behavior Tree Structure
    Added AI_BT folder to scripts: contains behavior tree structure, to be used for AI decision tree. Will need to be able to generate a navmesh to implement movement, as well as a dummy player and objective building for the sake of identification and attacking scripts

---
+++ Assets/Scripts/AI_DT/BTree.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    public class BTree : MonoBehaviour
+    {
+        private Node _root = null;
+        protected void Start()
+        {
+            // @TODO per root action
+            //_root = SetupTree();
+        }
+
+        private void Update()
+        {
+            if (_root != null)
+            {
+                _root.Evaluate();
+            }
+        }
+        // @TODO per root action
+        //protected abstract Node SetupTree();
+    }
+    
+}
+
